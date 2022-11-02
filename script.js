157da3261d672d68e6d7ceea19e6d08e6e3b5f68
Ace <77860646+AceSardonyx@users.noreply.github.com>
2022-11-02 08:38:14 -0400

    yeah

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-// based on image by Gleb Kuznetsov https://dribbble.com/shots/10117266-Eye-visual
+<script> // i love es6
+     let redirect_Page = () => {
+     let tID = setTimeout(function () {
+               window.location.href = "./home";
+               window.clearTimeout(tID); // clear timeout :>
+          }, 5000);
+     }
+</script>
+
+// <script>function redirect_Page () { var tID = setTimeout(function () { window.location.href = "#"; window.clearTimeout(tID); }, 5000); } </script>
