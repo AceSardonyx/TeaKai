160a62408c02574cf518669dd375d14a5d7c1945
Ace <AsierDavis@gmail.com>
2022-11-02 20:11:10 -0400

    add

---
+++ sys/h/script.js
@@ -0,0 +1,12 @@
+/*<script> // i love es6
+     let redirect_Page = () => {
+     let timahIDohyeah = setTimeout(function () {
+               window.location.href = "./home";
+               window.clearTimeout(timahIDohyeah); // clear timeout :>
+          }, 5000);
+     }
+</script>
+yeah, lets not use this because html!!!! YAYYY!!!
+*/ 
+
+// <script>function redirect_Page () { var tID = setTimeout(function () { window.location.href = "#"; window.clearTimeout(tID); }, 5000); } </script>
